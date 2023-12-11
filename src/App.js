a32139112e6779425e824921c964e9c1627d223c
Mahesh Mokale <maheshmokale.mm@gmail.com>
2023-12-10 19:21:54 -0800

    Pushing the authentication changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react"
+function App({ signOut, email }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mahesh <code>Mahesh</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo"></Image>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
